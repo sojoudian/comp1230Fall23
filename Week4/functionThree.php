8812514a9d49bb2ba3d691fbdfe4de7e33f6c59d
Maziar Sojoudian <maziar@Nagini.local>
2023-09-27 08:45:38 -0400

    third function

---
+++ Week4/functionThree.php
@@ -0,0 +1,13 @@
+<?php
+function sum($num1, $num2, $num3) {
+    $total = $num1 + $num2 + $num3;
+    return $total;
+}
+
+// Call the function with three arguments and store the result in a variable
+$result = sum(5, 10, 15);
+
+// Print the result
+echo "The sum of the numbers is: $result"; // Output: The sum of the numbers is: 30
+
+?>
