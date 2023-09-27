feca532382c5d23b3582adaae47975bf9c4df63c
Maziar Sojoudian <maziar@Nagini.local>
2023-09-27 08:36:05 -0400

    second function

---
+++ Week4/funcTwo.php
@@ -0,0 +1,8 @@
+<?php
+function greet($name, $age, $city) {
+    echo "Hello, my name is $name. I am $age years old and I live in $city.";
+}
+
+// Call the function with three arguments
+greet("John", 30, "New York");
+?>
