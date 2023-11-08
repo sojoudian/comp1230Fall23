d1d6600e4a2757edb0562589cb7cbfb63af17c0c
Maziar <sojodyan@gmail.com>
2023-11-08 09:51:08 -0500

    Update one.php

--- Week10/one.php
+++ Week10/one.php
@@ -8,6 +8,7 @@ class Book{
     const HARD_COVER=1;
     const SOFT_COVER=2;
     // echo "Book Cover type: " . Book::HARD_COVER;
+    // https://github.com/sojoudian/comp1230Fall23
 
 
     public function __construct($title, $author, $isbn){
@@ -35,7 +36,22 @@ class Book{
         //CTL7
 
     }
+    public function __set($name, $value){
+        if(method_exists($this, $name)){
+            $this->$method($value)
+        }
+    }
 
+    public function __get($name){
+        if(method_exists($this, $method)){
+            return $this->$method();
+        }
+        return null;
+    }
+
+    // use case: outside of the class
+    //$book->title = "Harry Potter";
+    //echo $book->title;
 
 }
 
