f36228fb47d8d231dd5411651c29e4dcf8afb332
Maziar Sojoudian <maziar@sojoudian.net>
2023-11-22 08:53:42 -0500

    Solution for Week 12 Lab, Q2

--- Week12/lab/1.php
+++ Week12/lab/1.php
@@ -20,3 +20,17 @@ $dog->makeSound(); // Output: Bark
 
 $cat = new Cat();
 $cat->makeSound(); // Output: Meow
+
+class Logger {
+    final public function writeLog($message) {
+        echo "Log: $message\n";
+    }
+}
+
+class FileLogger extends Logger {
+    // Trying to override writeLog will result in an error
+}
+
+$fileLogger = new FileLogger();
+$fileLogger->writeLog("This is a test log."); // Output: Log: This is a test log.
+
