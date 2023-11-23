bca9d7b34c78abff2aff7c1d276a7352fa8895a4
Maziar Sojoudian <maziar@sojoudian.net>
2023-11-23 08:44:58 -0500

    add part 2

---
+++ Week12/lab/2.php
@@ -0,0 +1,14 @@
+<?php 
+class Logger {
+    final public function writeLog($message) {
+        echo "Log: $message\n";
+    }
+}
+
+class FileLogger extends Logger {
+    // Trying to override writeLog will result in an error
+}
+
+$fileLogger = new FileLogger();
+$fileLogger->writeLog("This is a test log."); // Output: Log: This is a test log.
+?>
