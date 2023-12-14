8c2ccf7e8d9e588b74f883971f39bc81660c091f
Maziar Sojoudian <maziar@sojoudian.net>
2023-12-14 09:02:54 -0500

    update process.php

--- Week15/Q2/process.php
+++ Week15/Q2/process.php
@@ -3,4 +3,7 @@
         $input = htmlspecialchars($_POST['user_input']);
         echo " User input is: ". $input;
     }
+    // https://github.com/sojoudian/
+    // https://github.com/sojoudian/comp1230Fall23/
+    // https://github.com/sojoudian/comp1230Fall23/tree/master/Week15/Q2
 ?>
